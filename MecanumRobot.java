dc322b726f725b64f6fff5bb1a08381ed6f0b928
calam1tyc <99464669+calam1tyc@users.noreply.github.com>
2022-02-17 19:50:12 -0800

    Add two movement systems to test

--- MecanumRobot.java
+++ MecanumRobot.java
@@ -6,11 +6,11 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class MecanumRobot extends Robot {
-    private DcMotor leftFrontMotor;
-    private DcMotor leftBackMotor;
-    private DcMotor rightFrontMotor;
-    private DcMotor rightBackMotor;
-    private DcMotor[] motors;
+    private final DcMotor leftFrontMotor;
+    private final DcMotor leftBackMotor;
+    private final DcMotor rightFrontMotor;
+    private final DcMotor rightBackMotor;
+    private final DcMotor[] motors;
 
     public MecanumRobot(HardwareMap hardwareMap) {
         super(4, hardwareMap);
@@ -21,7 +21,7 @@ public class MecanumRobot extends Robot {
         motors = new DcMotor[]{leftFrontMotor, leftBackMotor, rightFrontMotor, rightBackMotor};
     }
 
-    public void moveInches(float xInches, float yInches) {
+    public void diagonalMoveInches(float xInches, float yInches, float speed) {
         float diagonal = (float)Math.sqrt(xInches * xInches + yInches * yInches);
         float leftFrontPower = yInches + xInches;
         float leftBackPower = yInches - xInches;
@@ -32,7 +32,7 @@ public class MecanumRobot extends Robot {
         double max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
         max = Math.max(max, Math.abs(leftBackPower));
         max = Math.max(max, Math.abs(rightBackPower));
-        max /= Robot.DRIVE_SPEED;
+        max /= speed;
 
         if (max > 1.0) {
             leftFrontPower  /= max;
@@ -49,6 +49,7 @@ public class MecanumRobot extends Robot {
         }
 
         while (leftFrontMotor.isBusy() && leftBackMotor.isBusy() && rightFrontMotor.isBusy() && rightBackMotor.isBusy()) {
+            telemetry.addData("Moving", true);
             telemetry.update();
         }
 
@@ -56,5 +57,46 @@ public class MecanumRobot extends Robot {
             motors[i].setPower(0);
             motors[i].setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         }
+    }
+    public void turnDeg(float deg, float speed) {
+        float inches = DIAMETER_INCHES * (float)Math.PI / (float)(WHEEL_DIAMETER_INCHES * Math.PI) * deg/360f;
+
+        leftFrontMotor.setTargetPosition(leftFrontMotor.getCurrentPosition() + (int)(inches * COUNTS_PER_INCH));
+        leftBackMotor.setTargetPosition(leftBackMotor.getCurrentPosition() + (int)(inches * COUNTS_PER_INCH));
+        rightFrontMotor.setTargetPosition(rightFrontMotor.getCurrentPosition() - (int)(inches * COUNTS_PER_INCH));
+        rightBackMotor.setTargetPosition(rightBackMotor.getCurrentPosition() - (int)(inches * COUNTS_PER_INCH));
+
+        for (DcMotor motor : motors) {
+            motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            motor.setPower(Math.abs(speed));
+        }
+
+        while (leftFrontMotor.isBusy() && leftBackMotor.isBusy() && rightFrontMotor.isBusy() && rightBackMotor.isBusy()) {
+            telemetry.addData("Turning", true);
+            telemetry.update();
+        }
+
+        for (DcMotor motor : motors) {
+            motor.setPower(0);
+            motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        }
+    }
+    public void turnToMoveInches(float xInches, float yInches, float turnSpeed, float speed) {
+        float deg = (float)(Math.atan(xInches/yInches) * 180/Math.PI);
+        turnDeg(deg, turnSpeed);
+        float diagonal = (float)(Math.sqrt(xInches * xInches + yInches * yInches));
+        for (DcMotor motor:motors) {
+            motor.setTargetPosition(motor.getCurrentPosition() + (int)(diagonal * COUNTS_PER_INCH));
+            motor.setPower(speed);
+        }
+        while (leftFrontMotor.isBusy() && leftBackMotor.isBusy() && rightFrontMotor.isBusy() && rightBackMotor.isBusy()) {
+            telemetry.addData("Moving", true);
+            telemetry.update();
+        }
+        for (DcMotor motor:motors) {
+            motor.setPower(0);
+            motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        }
+        turnDeg(-deg, turnSpeed);
     }
 }
